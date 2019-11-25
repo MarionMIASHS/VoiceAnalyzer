35b74c8b3660cf2434176e23d0feb4684c3e24f9
Marion Kissous <mkissous@personnel.mines-ales.fr>
2019-11-25 14:50:04 +0100

    Mise à jour texte à lire
    Remplacement de la fable par la description de sa journée.

--- app/src/main/java/com/functionality/voiceanalyzer/ParametersCollection.java
+++ app/src/main/java/com/functionality/voiceanalyzer/ParametersCollection.java
@@ -81,27 +81,9 @@ public class ParametersCollection {
             "« J’accepte de participer à cette présente recherche ce qui présuppose d’utiliser les applications demandées " +
             "(StepAnalyzer, LocationTracker & VoiceAnalyzer) ainsi que de répondre aux questionnaires pour lesquels je " +
             "serais sollicité(e)».\n";
-    public static final String TEXT_TO_READ = "Le Corbeau et le Renard\n" +
-            "Maître Corbeau, sur un arbre perché,\n"+
-            "Tenait en son bec un fromage.\n"+
-            "Maître Renard, par l'odeur alléché,\n"+
-            "Lui tint à peu près ce langage :\n"+
-            "Et bonjour, Monsieur du Corbeau,\n"+
-            "Que vous êtes joli ! que vous me semblez beau !\n"+
-            "Sans mentir, si votre ramage\n"+
-            "Se rapporte à votre plumage,\n"+
-            "Vous êtes le Phénix des hôtes de ces bois.\n"+
-            "À ces mots le Corbeau ne se sent pas de joie,\n"+
-            "Et pour montrer sa belle voix,\n"+
-            "Il ouvre un large bec, laisse tomber sa proie.\n"+
-            "Le Renard s'en saisit, et dit : Mon bon Monsieur,\n"+
-            "Apprenez que tout flatteur\n"+
-            "Vit aux dépens de celui qui l'écoute.\n"+
-            "Cette leçon vaut bien un fromage sans doute.\n"+
-            "Le Corbeau honteux et confus\n"+
-            "Jura, mais un peu tard, qu'on ne l'y prendrait plus.\n"+
-                "Jean de La Fontaine";
-    public static final String TITLE_READ = "It's time to read !";
+    public static final String TEXT_TO_READ = "L'enregistrement se lance automatiquement.\n" +
+            "Pour l'arrêter il vous suffit de revenir en arrière et d'appuyer sur le bouton Stop Recording.";
+    public static final String TITLE_READ = "Merci de décrire votre journée en quelques phrases.\n";
     public static String FILE_NAME = "VoiceAnalyzer";
     public static final String INFO_LEGAL = "Capteurs sensori-moteurs sur smartphone et vie quotidienne\n"+
             "Thèse en informatique\n" +
